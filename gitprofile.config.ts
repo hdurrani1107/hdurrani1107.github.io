6e1e5e04f197b93d33a18b3e05b2da45cd75d6bb
Humzah Durrani <hhd8@bu.edu>
2024-10-05 16:17:32 -0400

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -2,7 +2,7 @@
 
 const CONFIG = {
   github: {
-    username: 'arifszn', // Your GitHub org/user name. (This is the only required config)
+    username: 'hdurrani1107', // Your GitHub org/user name. (This is the only required config)
   },
   /**
    * If you are deploying to https://<USERNAME>.github.io/, for example your repository is at https://github.com/arifszn/arifszn.github.io, set base to '/'.
@@ -25,7 +25,7 @@ const CONFIG = {
       },
       manual: {
         // Properties for manually specifying projects
-        projects: ['arifszn/gitprofile', 'arifszn/pandora'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
+        projects: [''], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
       },
     },
     external: {
@@ -52,14 +52,14 @@ const CONFIG = {
     },
   },
   seo: {
-    title: 'Portfolio of Ariful Alam',
+    title: 'Portfolio of Humzah Durrani',
     description: '',
-    imageURL: '',
+    imageURL: 'https://drive.google.com/file/d/1SVLEhfhKqTM2BIjIRThksqu2i6h4HZqN/view?usp=sharing',
   },
   social: {
-    linkedin: 'ariful-alam',
-    twitter: 'arif_szn',
-    mastodon: 'arifszn@mastodon.social',
+    linkedin: 'www.linkedin.com/humzahdurrani',
+    twitter: '',
+    mastodon: '',
     researchGate: '',
     facebook: '',
     instagram: '',
@@ -69,71 +69,70 @@ const CONFIG = {
     udemy: '',
     dribbble: '',
     behance: '',
-    medium: 'arifszn',
-    dev: 'arifszn',
+    medium: '',
+    dev: '',
     stackoverflow: '', // example: '1/jeff-atwood'
     skype: '',
     telegram: '',
-    website: 'https://www.arifszn.com',
+    website: 'https://hdurrani.com',
     phone: '',
-    email: 'arifulalamszn@gmail.com',
+    email: 'durranihumzah@gmail.com',
   },
   resume: {
     fileUrl:
-      'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
+      'https://drive.google.com/file/d/1p-YlISNB_N8xuas1WN9XYt_DvUwVnTm2/view?usp=sharing', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
-    'PHP',
-    'Laravel',
-    'JavaScript',
-    'React.js',
-    'Node.js',
-    'Nest.js',
-    'MySQL',
-    'PostgreSQL',
-    'Git',
-    'Docker',
-    'PHPUnit',
-    'CSS',
-    'Antd',
-    'Tailwind',
+    'Python',
+    'HTML',
+    'Certified Solidworks Professional',
+    'Microsoft Suite',
+    'Prototyping',
+    'Driven',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
   ],
   experiences: [
     {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'September 2021',
-      to: 'Present',
-      companyLink: 'https://example.com',
+      company: 'Stanley Black and Decker',
+      position: 'Manufacturing Engineering Intern',
+      from: 'June 2023',
+      to: 'August 2023',
+      companyLink: 'https://www.stanleyblackanddecker.com/',
     },
     {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'July 2019',
-      to: 'August 2021',
-      companyLink: 'https://example.com',
+      company: 'NMG Aerospace',
+      position: 'Manufacturing Engineering Intern',
+      from: 'January 2022',
+      to: 'August 2022',
+      companyLink: 'https://www.nmgaerospace.com/',
     },
   ],
   certifications: [
     {
-      name: 'Lorem ipsum',
-      body: 'Lorem ipsum dolor sit amet',
+      name: 'Certified Solidworks Professional',
+      body: '',
       year: 'March 2022',
       link: 'https://example.com',
     },
   ],
   educations: [
     {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2015',
-      to: '2019',
+      institution: 'Boston University',
+      degree: 'M.S. Robotics and Autonomous Systems',
+      from: 'September 2024',
+      to: 'Tentative: May 2025',
     },
     {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2012',
-      to: '2014',
+      institution: 'Case Western Reserve University',
+      degree: 'B.S. Mechanical Engineering',
+      from: '2019',
+      to: '2024',
     },
   ],
   publications: [
